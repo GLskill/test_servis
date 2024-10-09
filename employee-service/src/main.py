32f9f56c0caf5a407c64b4a787fba253e23ebf65
GLskill <Glebluz1488@gmail.com>
2024-10-09 12:31:11 +0200

    add main,dockers fails

---
+++ employee-service/src/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from src.controllers import employee_controller
+
+app = FastAPI()
+
+app.include_router(employee_controller.router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
